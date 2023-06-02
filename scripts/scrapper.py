45853519ff2de792a56cfeef83f567426d64138d
Sebastian <sebastianmarquez0@gmail.com>
2023-06-02 17:49:13 -0600

    possible races collector done

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
@@ -14,7 +15,8 @@ class F1Scrapper:
         self.f_driver.get(F1Scrapper.url)
         self.f_driver.maximize_window()
         self.original_window = self.f_driver.current_window_handle
-        self.__title_block = self.__get_title_block()
+        self.__title_block = None
+        self.__link_list = None
 
     def login(self):
         google_login_btt = self.__wait(
@@ -51,14 +53,15 @@ class F1Scrapper:
                 real_button.click()
                 break
         sleep(3)
+        #tec login
         user_input = self.__wait(
             self.f_driver,
-            lambda d: d.find_element(By.ID, 'Ecom_User_ID')
+            lambda d: d.find_element(By.ID, 'Ecom_User_ID'),
+            60
         )
         ActionChains(self.f_driver) \
             .send_keys_to_element(user_input, "a01632483") \
             .perform()
-        sleep(2)
         pass_input = self.__wait(
             self.f_driver,
             lambda d: d.find_element(By.ID, 'Ecom_Password')
@@ -70,6 +73,7 @@ class F1Scrapper:
             self.f_driver,
             lambda d: d.find_element(By.ID, 'submitButton')
         ).click()
+
         sleep(3)
         self.f_driver.switch_to.window(self.original_window)
         sleep(1)
@@ -83,7 +87,7 @@ class F1Scrapper:
             raise e
 
     def __get_title_block(self):
-        return self.__wait(
+        self.__title_block = self.__wait(
             self.f_driver,
             lambda d:
             d.find_element(By.ID, 'title_holder')
@@ -97,80 +101,94 @@ class F1Scrapper:
         )
 
     def __get_season_list(self):
-        spans = self.__wait(
+        links = self.__wait(
             self.__title_block,
             lambda d:
             d.find_element(By.ID, 'sel_season_tb')
             .find_elements(By.TAG_NAME, 'span')
         )
-        links = list(map(lambda span: self.__wait(span, lambda s: s.find_element(By.TAG_NAME, 'a')), spans))
         return links
 
+    def __get_link_list(self):
+        link_list = {}
+        useful_cities_2011 = ['Singapore', 'Korea', 'India', 'Abu-Dhabi', 'Brazil']
+        self.__get_title_block()
+        season_list = self.__get_season_list()
+        race_list_block = self.__get_race_list()
+        num_seasons = len(season_list)
+        for i in range(num_seasons):
+            self.__get_title_block()
+            season_list = self.__get_season_list()
+            race_list_block = self.__get_race_list()
+            span = season_list[i]
+            sleep(0.5)
+            link = self.__wait(span, lambda s: s.find_element(By.TAG_NAME, 'a'))
+            link_year = int(link.text)
+            if link_year < 2011:
+                continue
+            link_list[link_year] = {}
+            link.click()
+            self.__wait(self.__title_block, EC.visibility_of(race_list_block))
+            ActionChains(self.f_driver) \
+                .move_to_element(race_list_block) \
+                .perform()
+            races_links = race_list_block.find_elements(By.TAG_NAME, 'a')
+            for race in races_links:
+                race_name = race.text
+                if link_year == 2011 and race_name not in useful_cities_2011:
+                    continue
+                link_list[link_year][race_name] = False
+        self.__link_list = link_list
+
+
     def run(self):
-        season_list = scrapper.__get_season_list()
-        race_list_block = scrapper.__get_race_list()
+        if self.__link_list is None:
+            self.__get_link_list()
+        # while () {
+        #
+        # }
+        # self.__get_title_block()
+        # useful_cities_2011 = ['Singapore', 'Korea', 'India', 'Abu-Dhabi', 'Brazil']
+        # season_list = self.__get_season_list()
+        # race_list_block = self.__get_race_list()
+        #
+        #
+        #
+        #     race.click()
+        #     self.__position_to_lap()
 
-        link = season_list[0]
-        link.click()
-        self.__wait(self.__title_block, EC.visibility_of(race_list_block))
-        races_links = race_list_block.find_elements(By.TAG_NAME, 'a')
-        races_links[0].click()
+    def __position_to_lap(self):
         # at singapore 2011 laps are enabled
-
-    def get_current_lap_info(self):
-        sleep(10)
-        print('searching')
-        wrap = self.__wait(
+        select_ele = self.__wait(
             self.f_driver,
-            lambda d: d.find_element(By.ID, 'wrap'),
-            45
+            lambda d: d.find_element(By.ID, 'replay_laps_select')
         )
-        print('wrap done')
-        main_holder = self.__wait(
-            wrap,
-            lambda d: d.find_element(By.ID, 'main_holder'),
-            45
-        )
-        print('main_holder done')
-        main_left_holder = self.__wait(
-            main_holder,
-            lambda d: d.find_element(By.ID, 'main_left_holder'),
-            45
-        )
-        print('main_left_holder done')
-        stats_block = self.__wait(
-            main_left_holder,
-            lambda d: d.find_element(By.ID, 'stats_block'),
-            45
-        )
-        print('stats_block done')
-        stats_table = self.__wait(
-            stats_block,
-            lambda d: d.find_element(By.CLASS_NAME, 'stats_table'),
-            45
-        )
-        print('stats_table done')
-        print(stats_table.text)
-        for i in range(1 + 21):
-            stats_div = self.__wait(
-                stats_table,
-                lambda d: d.find_element(By.ID, f'stats_d_{0 if i < 10 else ""}{i}')
-            )
-            print(stats_div.text)
-            elements = [
-                'pos',
-                'nick',
-                'gap'
-            ]
+        select = Select(select_ele)
+        for opt in select.options:
+            if int(opt.text) < 3:
+                continue
+            opt.click()
+            sleep(0.5)
+            self.__get_current_lap_info()
+
+    def __get_current_lap_info(self):
+        elements = [
+            'pos',
+            'nick',
+            'gap'
+        ]
+        for i in range(1, 21):
+            print(f'getting: stats_d_{0 if i < 10 else ""}{i}')
+            print(f'Driver i: {i}')
             for element in elements:
                 val = self.__wait(
-                    stats_div,
+                    self.f_driver,
                     lambda d: d.find_element(By.ID, f'i_{0 if i < 10 else ""}{i}_{element}')
                 ).text
-                print(val)
+                print(f"\t{element}: {val}")
 
 
 scrapper = F1Scrapper()
 scrapper.login()
+sleep(1)
 scrapper.run()
-scrapper.get_current_lap_info()
